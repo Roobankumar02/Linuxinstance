4f971ccb825db0c82bf63d913f3592240339bce3
Rooban <roobankumarrs@gmail.com>
2024-08-04 18:33:18 +0000

    Edited in Branch

---
+++ file1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "Give a data"
+read a
+echo "Give b data"
+read b
+
+echo "Addition of a and b is $((a+b))"
+echo "Subtraction of a and b is $((a-b))"
+echo "Multiplication of a and b is $((a*b))"
+echo "Division of a and b is $((a/b))"
+echo "Modules of a and b is $((a%b))"
